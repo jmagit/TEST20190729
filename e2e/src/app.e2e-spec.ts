3e4a634b358dd97c8bdce007a50d25f318513169
Javier <jmartin@grupoloyal.com>
2019-07-30 11:29:31 +0200

    Multi it

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -14,30 +14,36 @@ describe('Para probar la página principal', () => {
   });
 
   describe('Deben funcionar las opciones del menu', () => {
-    let menu: Array<any>;
-    beforeAll(() => {
-      menu = [
+    let menu: Array<any> = [
         { ruta: '/inicio', texto: 'inicio', componente: 'app-home' },
         { ruta: '/demos', texto: 'demos', componente: 'app-demos' },
         { ruta: '/chisme/de/hacer/numeros', texto: 'calculadora', componente: 'calculadora' },
-        { ruta: '/personas', texto: 'personas', componente: 'app-personas-list' },
+        { ruta: '/personas', texto: 'personas', componente: 'xapp-personas-list' },
         { ruta: '/pepito/grillo', texto: 'pepito', componente: 'app-personas-view' },
         { ruta: '/config', texto: 'config', componente: 'app-configuracion' },
         { ruta: '/config/datos', texto: 'datos', componente: 'app-datos' },
       ];
+    beforeAll(() => {
       page = new AppPage();
       page.navigateTo();
       browser.sleep(1000);
     });
 
-    it('Navego a las opciones', () => {
-      menu.forEach(item => {
+    menu.forEach(item => {
+      it(`Navego a ${item.texto}`, () => {
         page.pulsarMenu(item.texto);
         expect(page.dameComponente(item.componente).isPresent()).toBeTruthy();
         browser.sleep(1000);
       });
     });
 
+    // it('Navego a las opciones', () => {
+    //   menu.forEach(item => {
+    //     page.pulsarMenu(item.texto);
+    //     expect(page.dameComponente(item.componente).isPresent()).toBeTruthy();
+    //     browser.sleep(1000);
+    //   });
+    // });
 
     // it('Navego a Demos', () => {
     //   page.pulsarMenu('demos');
